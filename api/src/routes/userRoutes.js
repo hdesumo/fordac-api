386cca79845e5908997f8a8afa04e2c5e1a49f11
hdesumo <honoredesumo@yahoo.com>
2025-11-14 13:18:25 +0100

    Suppression du dossier api/ obsolète

--- api/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import verifyToken from "../middlewares/verifyToken.js";
-import { getUsers } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.get("/", verifyToken, getUsers);
-
-export default router;
