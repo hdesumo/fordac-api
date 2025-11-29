455513532c8393b246fd85c3403e259d517972c5
hdesumo <honoredesumo@yahoo.com>
2025-11-29 20:09:50 +0100

    Force redeploy backend

--- src/routes/authAdminRoutes.js
+++ src/routes/authAdminRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const { loginAdmin } = require("../controllers/authAdminController");
+const { login } = require("../controllers/authAdminController");
 
-// Route de connexion admin
-router.post("/login", loginAdmin);
+router.post("/login", login);
 
 module.exports = router;
